b22f2588b88a3d56cc3ac9910a4adfb7cfbd6235
Konastantin Lishavsky <lishavsky95@mail.ru>
2024-09-17 23:07:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class InputConsoleDemo
+{
+    static void Main()
+    {
+        //Текстовая переменная
+        string name;
+
+        //Заголовок консольного окна
+        Console.Title = "Давайте познакомимся...";
+
+        //Сообщение в консоли:
+        Console.Write("Как вас зовут? ");
+
+        //Считывание текста
+        name=Console.ReadLine();
+
+        //Ещё одна текстовая переменная:
+        string txt = "Очень приятно, " + name + "!";
+
+        //Заголовок консольного окна 2:
+        Console.Title = "Знакомство состоялось!";
+
+        //Сообщение в консоли 2:
+        Console.WriteLine(txt);
+    }
+}
